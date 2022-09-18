56948afc40aa879643bc728eba3dc8d93f63d563
Julien Bourgeois <julienrenebourgeois@gmail.com>
2022-09-17 19:33:07 -0500

    Initial Commit

---
+++ Automatic MVP Attempt 1 WatchKit AppTests/Automatic_MVP_Attempt_1_WatchKit_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Automatic_MVP_Attempt_1_WatchKit_AppTests.swift
+//  Automatic MVP Attempt 1 WatchKit AppTests
+//
+//  Created by Julien Paid Developer on 9/17/22.
+//
+
+import XCTest
+@testable import Automatic_MVP_Attempt_1_WatchKit_Extension
+
+class Automatic_MVP_Attempt_1_WatchKit_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Automatic MVP Attempt 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Automatic MVP Attempt 1
+//
+//  Created by Julien Paid Developer on 9/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
